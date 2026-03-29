23564f27410ec294f6eeab9c551f12b058907fd0
djriad157764-creator <djriad157764@gmail.com>
2026-03-30 00:02:10 +0600

    Assignment 6 Initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-700 text-center">
+        Hello World ! Assignment 6 Initialize
+      </h1>
+    </>
+  );
+}
+
+export default App;
